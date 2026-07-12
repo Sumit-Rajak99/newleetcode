b00f607362571aef52858f27a6dc4f2d82edd5b4
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2026-07-12 18:50:11 +0530

    rank tranform of array

---
+++ rank.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayRankTransform(self, arr):
+        
+        sorted_arr = sorted(set(arr))
+        rank = {}
+        
+        for i, num in enumerate(sorted_arr):
+            rank[num] = i + 1
+
+        return [rank[num] for num in arr]
